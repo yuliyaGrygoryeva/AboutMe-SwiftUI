0c6b5c870cb35ebb43804f3ba5ad27db07beb17a
Yuliya Grygoryeva <jgrygoryeva@gmail.com>
2022-07-05 11:11:21 -0700

    Initial Commit

---
+++ AboutMe/AboutMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  AboutMeApp.swift
+//  AboutMe
+//
+//  Created by Yuliya  on 7/5/22.
+//
+
+import SwiftUI
+
+@main
+struct AboutMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
