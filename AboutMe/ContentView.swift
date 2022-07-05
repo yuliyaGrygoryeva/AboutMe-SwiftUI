41f076521497306c0e8d438a01d45f3e9f345529
Yuliya Grygoryeva <jgrygoryeva@gmail.com>
2022-07-05 14:12:39 -0700

    first commit

--- AboutMe/ContentView.swift
+++ AboutMe/ContentView.swift
@@ -11,11 +11,13 @@ struct ContentView: View {
     var body: some View {
         Text("Hello, world!")
             .padding()
+            .foregroundColor(.red)
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .previewInterfaceOrientation(.portrait)
     }
 }
